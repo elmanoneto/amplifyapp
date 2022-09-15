1148bd28b686c050673fe1987ac3e8f04003da1a
elmanoneto <elmanoneto@gmail.com>
2022-09-15 12:20:52 -0300

    Added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp17bbf472": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import logo from './logo.svg';
+import '@aws-amplify/ui-react/styles.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <>
+		<Authenticator>
+			{({ signOut, user }) => (
+				<div className="App">
+					<header className="App-header">
+					<p>
+						Hey {user.username}, welcome to my channel, with auth!
+					</p>
+					<button onClick={signOut}>Sign out</button>
+					</header>
+				</div>
+			)}
+		</Authenticator>
+	</>
+	
   );
 }
 
